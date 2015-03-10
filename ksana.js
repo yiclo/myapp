65d2330fbbd5c8ebcc8728f7b4201a625d8ddd74
yiclo <yiclo@sce.pccu.edu.tw>
2015-03-10 15:17:07 +0800

    first build

---
+++ ksana.js
@@ -0,0 +1,48 @@
+jsonp_handler({
+ "version": "1",
+ "build": 2,
+ "title": "myapp",
+ "minruntime": 1,
+ "baseurl": "http://rawgit.com/yiclo/myapp/master/",
+ "description": "",
+ "browserify": {
+  "external": [
+   "react",
+   "bootstrap"
+  ]
+ },
+ "files": [
+  "index.html",
+  "index.css",
+  "react-bundle.js",
+  "bundle.js",
+  "jquery.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css",
+  "sample.kdb"
+ ],
+ "filesizes": [
+  547,
+  64,
+  652507,
+  146450,
+  277980,
+  380,
+  35607,
+  113502,
+  97542
+ ],
+ "filedates": [
+  "2015-03-10T06:55:11.085Z",
+  "2015-03-10T06:55:11.085Z",
+  "2015-03-10T06:55:11.195Z",
+  "2015-03-10T06:56:27.245Z",
+  "2015-03-10T06:55:11.135Z",
+  "2015-03-10T06:55:11.145Z",
+  "2015-03-10T06:55:11.045Z",
+  "2015-03-10T06:55:11.045Z",
+  "2015-03-10T06:55:41.995Z"
+ ],
+ "date": "2015-03-10T06:56:28.208Z"
+})
