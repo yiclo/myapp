894f224ada6b4303cecf21d3ea70b67f487e8f9c
yiclo <yiclo@sce.pccu.edu.tw>
2015-03-10 16:26:43 +0800

    third build

--- bundle.js
+++ bundle.js
@@ -11,7 +11,7 @@ var React=require("react");
 var kse=require("ksana-search");
 var maincomponent = React.createClass({displayName: "maincomponent",
   getInitialState:function() {
-    return {result:[],tofind:"資生"};
+    return {result:[],tofind:"君子"};
   },
   search:function() {
     kse.search("sample",this.state.tofind,{range:{start:0}},function(err,data){
